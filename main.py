76019230a42e6fb36b76e09d963a996f3839f469
Harshitha <harshithakomatireddy02@gmail.com>
2026-09-20 09:55:14 +0530

    Fix CORS

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from pydantic import BaseModel
 app = FastAPI(title="Sahaay Backend")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
